92e478effbd8ed921376435a3aca5d73a3db526d
Thomas Vanderhaeghen <thomas@optios.net>
2022-04-29 14:37:56 +0200

    Fixed Teneo transport factory tests

--- Transport/TeneoTransportFactory.php
+++ Transport/TeneoTransportFactory.php
@@ -2,6 +2,7 @@
 
 namespace Optios\Mailer\Teneo\Transport;
 
+use Symfony\Component\Mailer\Exception\UnsupportedSchemeException;
 use Symfony\Component\Mailer\Transport\AbstractTransportFactory;
 use Symfony\Component\Mailer\Transport\Dsn;
 use Symfony\Component\Mailer\Transport\TransportInterface;
@@ -10,6 +11,10 @@ final class TeneoTransportFactory extends AbstractTransportFactory
 {
     public function create(Dsn $dsn): TransportInterface
     {
+        if (!\in_array($dsn->getScheme(), $this->getSupportedSchemes(), true)) {
+            throw new UnsupportedSchemeException($dsn, 'teneo', $this->getSupportedSchemes());
+        }
+
         $username = $this->getUser($dsn);
         $password = $this->getPassword($dsn);
         $host     = 'default' === $dsn->getHost() ? null : $dsn->getHost();
